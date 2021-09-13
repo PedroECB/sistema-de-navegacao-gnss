013f2cd80b92d4bbf4dec2abb5adf3e38c467251
PeuGomes <pedroivobarretogomes26@gmail.com>
2021-09-13 20:22:27 -0300

    Tela Inicial

--- app/src/main/java/com/example/trabalhosemestral/MainActivity.java
+++ app/src/main/java/com/example/trabalhosemestral/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.trabalhosemestral;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
