2dcaa80745473ce314b3cecc3b0a94e53bc1db90
ylmoura <yvesluan016@gmail.com>
2021-09-24 14:36:45 -0300

    tela de credito

--- app/src/main/java/com/example/trabalhosemestral/CreditoActivity.java
+++ app/src/main/java/com/example/trabalhosemestral/CreditoActivity.java
@@ -1,14 +1,17 @@
-7package com.example.trabalhosemestral;
+package com.example.trabalhosemestral;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.trabalhosemestral.R;
 
 public class CreditoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       /*  setContentView(R.layout.activity_credito); */
         setContentView(R.layout.activity_credito);
     }
 }
