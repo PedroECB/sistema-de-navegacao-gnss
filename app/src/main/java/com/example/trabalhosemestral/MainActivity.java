57e0528477695a80d8c28daa85b5f8d0d25354db
Pedro Carvalho <pedrosophbc@gmail.com>
2021-10-20 23:56:03 -0300

    Adicionando atividade de mapa

--- app/src/main/java/com/example/trabalhosemestral/MainActivity.java
+++ app/src/main/java/com/example/trabalhosemestral/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
@@ -16,9 +17,12 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button botaoNavegacao = (Button) findViewById(R.id.button_telanavegacao);
         Button botaoConfiguracao = (Button) findViewById(R.id.button_telaconfiguracao);
         Button botaoCreditos = (Button) findViewById(R.id.button_telacreditos);
 
+
+        botaoNavegacao.setOnClickListener(this);
         botaoConfiguracao.setOnClickListener(this);
         botaoCreditos.setOnClickListener(this);
     }
@@ -26,8 +30,12 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     @Override
     public void onClick(View view) {
 
-
         switch (view.getId()){
+            case R.id.button_telanavegacao:
+                Intent navegacao = new Intent(this, MapsActivity.class);
+                startActivity(navegacao);
+                break;
+
             case R.id.button_telaconfiguracao:
                 Intent configTela = new Intent(this, ConfigActivity.class);
                 startActivity(configTela);
@@ -37,6 +45,9 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                 Intent configCredito = new Intent(this, CreditoActivity.class);
                 startActivity(configCredito);
                 break;
+
+
+
         }
     }
 }
